98b5de94676c3fb567db51011bd7e0bd0bf3d450
WorldOfBerk <baris.berk.sengl@hotmail.com>
2024-07-28 21:24:26 +0200

    basic api handler completed

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,4 +1,5 @@
 #include "gui/mainwindow.h"
+#include <iostream>
 
 MainWindow::MainWindow() {
     shape = sf::CircleShape(50);
@@ -8,6 +9,14 @@ MainWindow::MainWindow() {
         {100, 100}, {200, 200}, {300, 100}, {400, 200}
     };
     mapDrawer.setRoute(route);
+
+    // Simulate initial engine data update
+    engineData.update(0.0f, 300.0f, 10000.0f);
+    std::cout << engineData.getStatus() << std::endl;
+
+    // Get weather data for Berlin
+    std::string weatherData = ApiHandler::getWeatherData(52.52, 13.41, "1949-12-28", "1949-12-31");
+    std::cout << weatherData << std::endl;
 }
 
 void MainWindow::draw(sf::RenderWindow &window) {
--- src/utils/api_handler.cpp
+++ src/utils/api_handler.cpp
@@ -1,5 +1,30 @@
-//
-// Created by b_sen on 09.07.2024.
-//
+#include "utils/api_handler.h"
+#include <curl/curl.h>
+#include <iostream>
+#include <sstream>
 
-#include "api_handler.h"
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+std::string ApiHandler::getWeatherData(double latitude, double longitude, const std::string &startDate, const std::string &endDate) {
+    std::string readBuffer;
+    CURL* curl = curl_easy_init();
+    if (curl) {
+        std::ostringstream urlStream;
+        urlStream << "https://archive-api.open-meteo.com/v1/archive?latitude=" << latitude
+                  << "&longitude=" << longitude
+                  << "&start_date=" << startDate
+                  << "&end_date=" << endDate
+                  << "&hourly=temperature_2m&timezone=Europe%2FBerlin";
+        std::string url = urlStream.str();
+
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+    return readBuffer;
+}
